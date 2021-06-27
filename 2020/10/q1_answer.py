0535fd40c010e0b629a9bd6fbee48c74510766d4
Nico Castro <ncastro@nypublicradio.org>
2021-06-27 13:29:43 -0400

    day 10 q1 done

--- 2020/10/q1_answer.py
+++ 2020/10/q1_answer.py
@@ -1,7 +1,23 @@
 #!/usr/local/bin/python3
 
+def parse_and_sort(name: str) -> []:
+    jolts = [0, ]
+    with open(name, 'r') as f:
+        for line in f:
+            jolts.append(int(line.rstrip()))
+    jolts.sort()
+    jolts.append(jolts[-1] + 3)
+    return jolts
+
 def main():
-    print("Hello World!")
+    joltages = parse_and_sort('input.txt')
+    jumps = {}
+
+    for i in range(len(joltages)-1):
+        jump = joltages[i+1] - joltages[i]
+        jumps[jump] = jumps.get(jump, 0) + 1
+    
+   print(jumps[1] * jumps[3])
 
 if __name__ == "__main__":
     main()
